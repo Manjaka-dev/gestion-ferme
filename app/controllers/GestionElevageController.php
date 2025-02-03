86cdc4ce0fbff5ad05dcd68134e01948777c4063
AinaSatamandresy <aina.satamandresy.com>
2025-02-03 21:13:00 +0100

    creation des fonctionnalité pour GestionElevage

---
+++ app/controllers/GestionElevageController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\GestionElevageModel;
+use Flight;
+
+class GestionElevageController {
+
+    // Fonction pour obtenir la situation de l'élevage
+    public function getSituationElevage() {
+        $date = isset($_GET['date']) ? $_GET['date'] : date('Y-m-d');
+
+        $gestionElevageModel = new GestionElevageModel(Flight::db());
+
+        $situationElevage = $gestionElevageModel->getSituationElevage($date);
+        
+        $ventes = $gestionElevageModel->getVentesElevage($date);
+        
+        $depenses = $gestionElevageModel->getDepensesElevage($date);
+
+        $gains = $gestionElevageModel->getGainsElevage($date);
+
+        $conclusion = $gestionElevageModel->getConclusionElevage($date);
+
+        $data = [
+            'situationElevage' => $situationElevage,
+            'ventes' => $ventes,
+            'depenses' => $depenses,
+            'gains' => $gains,
+            'conclusion' => $conclusion,
+            'date' => $date
+        ];
+
+        Flight::render('situation-elevage', $data);
+    }
+
+     // Fonction pour obtenir l'historique des transactions
+     public function getHistoriqueTransa() {
+        $gestionElevageModel = new GestionElevageModel(Flight::db());
+
+        $historiqueTransactions = $gestionElevageModel->getHistoriqueTransactions();
+
+        $data = [
+            'historiqueTransactions' => $historiqueTransactions
+        ];
+        Flight::render('historique-transactions', $data);
+    }
+}
+?>

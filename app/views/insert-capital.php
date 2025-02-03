289086cf48677aec77ec4e0f1bf522cb241429da
Faniry-lang <faniryranaivoson167@gmail.com>
2025-02-03 19:38:49 +0300

    front insertion

---
+++ app/views/insert-capital.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=de:vice-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Insertion capital</title>
+</head>
+<body>
+    <div class="container">
+        <div class="form rounded-lg">
+            <form action="">
+                <div class="intitule">
+                    <h1>Insérer capital: </h1>
+                </div>
+                <p>
+                    <h3>Montant: </h3>
+                    <input type="number">
+                </p>
+                <button type="submit" class="validation-btn rounded-md">Valider</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
